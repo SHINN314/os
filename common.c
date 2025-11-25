05250e1c1f129c845a28a4b9d5850b22c467f329
SHINN314 <shinnosukekimura8@gmail.com>
2025-11-25 21:14:14 +0900

    add: その他の標準ライブラリを実装

--- common.c
+++ common.c
@@ -69,3 +69,40 @@ void printf(const char *fmt, ...) {
     end:
         va_end(vargs);
 }
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    /* srcからdstへnバイト分コピーする関数 */
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while(n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    /* bufの先頭からnバイト分をcで埋める関数 */
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+    /* srcからdstへ文字列をコピーする関数 */
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    whilie (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
