19eb9eca3af767dff908224405d459e1f6c56a6c
SHINN314 <shinnosukekimura8@gmail.com>
2025-11-25 20:02:36 +0900

    update: 自作pirntfでHello worldと演算結果をコンソールに出力

--- kernel.c
+++ kernel.c
@@ -1,4 +1,5 @@
 #include "kernel.h"
+#include "common.h"
 
 extern char __bss[], __bss_end[], __stack_top[];
 
@@ -27,7 +28,8 @@ void putchar(char ch) {
 
 void kernel_main(void) {
     // Hello World!を出力する
-    const char *s = "\n\nHello World!\n";
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
     for (int i = 0; s[i] != '\0'; i++) {
         putchar(s[i]);
     }
