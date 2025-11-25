0e83ac0139acd19444770b60b404ae249f2a210d
SHINN314 <shinnosukekimura8@gmail.com>
2025-11-25 20:00:51 +0900

    chore: コメントをついか

--- common.c
+++ common.c
@@ -9,14 +9,17 @@ void printf(const char *fmt, ...) {
     while(*fmt) {
         if (*fmt == '%') {
             fmt++;
-            switch (*fmt == '%') {
+
+            switch (*fmt) {
                 case '\0':
                     putchar('%');
                     goto end;
                 case '%':
+                    // '%%'は'%'を出力する
                     putchar('%');
                     break;
                 case 's': {
+                    // 文字列を出力する
                     const char *s = va_arg(vargs, const char *);
                     while (*s) {
                         putchar(*s);
@@ -27,19 +30,23 @@ void printf(const char *fmt, ...) {
                 case 'd': {
                     int value = va_arg(vargs, int);
                     unsigned magnitude = value;
+
+                    // 負の数の場合は符号を出力して絶対値に変換
                     if (value < 0) {
                         putchar('-');
                         magnitude = -magnitude;
                     }
 
+                    // 桁数をカウントする
                     unsigned divisor = 1;
                     while (magnitude / divisor > 9)
                         divisor *= 10;
 
+                    // 桁数を出力する
                     while (divisor > 0) {
                         putchar('0' + magnitude / divisor);
-                        magnitude %= divisor;
-                        divisor /= 10;
+                        magnitude %= divisor; // 次の桁へ移動する
+                        divisor /= 10; // 桁を一つ減らす
                     }
 
                     break;
@@ -48,7 +55,7 @@ void printf(const char *fmt, ...) {
                     unsigned value = va_arg(vargs, unsigned);
                     for (int i = 7; i >= 0; i--) {
                         unsigned nibble = (value >> (i * 4)) & 0xf;
-                        putchar("0123456789abcdef"[nibble]);
+                        putchar("0123456789abcdef"[nibble]); // 16進数の文字を出力
                     }
                 }
             }
